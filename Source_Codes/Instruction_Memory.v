cc156a27fcedab9dded518dc402c142ffc6a6a05
EmJunaid <93525537+EmJunaid@users.noreply.github.com>
2021-12-30 08:21:18 -0800

    Update Instruction_Memory.v

--- Source_Codes/Instruction_Memory.v
+++ Source_Codes/Instruction_Memory.v
@@ -6,7 +6,12 @@ module Instruction_Memory (
     reg [31:0] instructions_Value[31:0];
 
     initial begin
-       $readmemh("instruction.mem", instructions_Value);
+       //$readmemh("instruction.mem", instructions_Value);
+       instructions_Value[0] <= 32'h00000000;
+       instructions_Value[1] <= 32'h8CDEFAB7; //32'hFFC4A303; 0x8CDEFAB7
+       instructions_Value[2] <= 32'h0064A423;
+       instructions_Value[3] <= 32'h0062E233;
+       instructions_Value[4] <= 32'hFE420AE3;
     end
 
     always@(*) begin
